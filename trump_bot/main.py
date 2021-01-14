6037703aa017ea94106a5709fc9f55f3b00bba9b
Hakula Chen <i@hakula.xyz>
2021-01-15 03:07:42 +0800

    refactor: calculate procedure duration

--- trump_bot/main.py
+++ trump_bot/main.py
@@ -7,12 +7,15 @@ def main() -> None:
     Main function for bot.
     '''
 
+    current_time: float = time.time()
+
     c = corpus()
     c.get_all_text_data(all_in_one=True)
 
+    duration: float = time.time() - current_time
+    print(f'Reading dataset done: {duration:.2f} s')
+    current_time += duration
+
 
 if __name__ == '__main__':
-    start_time: float = time.time()
     main()
-    duration: float = time.time() - start_time
-    print(f'Done: {duration:.2f} s')
